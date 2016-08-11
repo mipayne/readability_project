84a135972dac3b543f2e958814dc11070f5cdd8f
Madelyn Payne <mpayne@mit.edu>
2016-08-11 16:36:38 -0400

    Improve MLU calculator

--- readability_project.py
+++ readability_project.py
@@ -206,83 +206,19 @@ for sent in sent_dict:
 #print lem_ready_word_list
 print final_words
 '''
-    
+'''
 f = open("./StoryCorpus_copy/Left_Right_Emma.txt")
 raw = f.read()
-
-
-final_words = []
-word_sent_token = []
-pos_tag_sent_list = []
-lem_ready_sent_list = []
-mlu_ready_list = []
-sent_dict = collections.OrderedDict()
-
-#tokenize
-sent_tokenize_list = sent_tokenize(raw)#breaks raw text into list of its sentences
-#breaks each sentence into a  list of its parts
-for sent in sent_tokenize_list:
-    word_sent_token.append(word_tokenize(sent))# DOWNSIDE: breaks up contractions\
-#creates list of tuples for each part of each sentence (word, pos_tag) 
-for sent in word_sent_token:
-    pos_tag_sent_list.append(nltk.pos_tag(sent))
-#print "POS_TAG"    
-#print pos_tag_sent_list
 '''
-#pos_tag
-for sent in pos_tag_sent_list:
-    #pos_tag_sent_list is a list of lists
-        #list_outside = sentences
-        #list_inside = (word,pos_tag)
-    for tuple_pair in sent:
-        word = tuple_pair[0]
-        pos_tag = tuple_pair[1]
-        #makes words lowercase
-        word = word.lower()
-        lem_ready_word_list.append((word, pos_tag))
-print 'LEM_READY'
-print lem_ready_word_list
-'''
- #pos_tag
-for sent in pos_tag_sent_list:
-    lem_ready_word_list = []
-    for tuple_pair in sent:
-        word = tuple_pair[0]
-        pos_tag = tuple_pair[1]
-        #gives correct pos_tag
-        wordnet_pos = get_wordnet_pos(pos_tag)
-        #makes words lowercase
-        word = word.lower()
-        lem_ready_word_list.append((word, wordnet_pos, pos_tag))
-    lem_ready_sent_list.append(lem_ready_word_list)
-#print lem_ready_sent_list
-        
- #lemmatize
-for sent in lem_ready_sent_list:
-    lem_sent = []
-    for tuple_trip in sent:
-        word = tuple_trip[0]
-        wordnet_pos = tuple_trip[1]
-        pos_tag = tuple_trip[2]
-        #makes sure words with no equivalent wordnet pos_tag still get lemmatized
-        if wordnet_pos == '':
-            lem_word = wordnet_lemmatizer.lemmatize(word)
-            if type(lem_word) == unicode:
-                lem_word = unidecode(lem_word)
-            lem_sent.append((word, lem_word, pos_tag))
-            #final_words.append((lem_word, pos_tag))
-        
-            #insert code to assign tags according to WordCategories.xlsx (ex: negatives, pronoun)
-    #general lemmatization
-        else:
-            lem_word = wordnet_lemmatizer.lemmatize(word, pos=wordnet_pos)
-            if type(lem_word) == unicode:
-                lem_word = unidecode(lem_word)
-            lem_sent.append((word, lem_word, pos_tag))
-            #final_words.append((lem_word, pos_tag))
-    mlu_ready_list.append(lem_sent)
-#print mlu_ready_list
-#book_words[fileName] = final_words
+
+
+import glob
+list_of_files = glob.glob('resources/converted/StoryCorpus/*.txt')
+
+MLU_dict = {}
+
+morpheme1_list = []
+VBDN_list = []
 NNS_list = []
 VBN_list = []
 VBD_list = []
@@ -291,43 +227,262 @@ VBG_list = []
 POS_list = []
 WP_list = []
 be_list = []
-    
-for sent in mlu_ready_list:
-    for tuple_trip in sent:
-        word = tuple_trip[0]
-        lem_word = tuple_trip[1]
-        pos_tag = tuple_trip[2]
-        if pos_tag == 'NNS':
-            NNS_list.append(tuple_trip)
-        if pos_tag == 'VBN':
-            VBN_list.append(tuple_trip)
-        if pos_tag == 'VBD':
-            VBD_list.append(tuple_trip)
-        if pos_tag == 'VBZ':
-            VBZ_list.append(tuple_trip)
-        if pos_tag == 'VBG':
-            VBG_list.append(tuple_trip)
-        if pos_tag == 'POS':
-            POS_list.append(tuple_trip)
-        if pos_tag == 'WP$':
-            WP_list.append(tuple_trip)
-        if lem_word == 'be':
-            be_list.append(tuple_trip)
+#mlu_ready_list = []
+#morpheme_count = 0
+#sent_count = 0
+
+for fileName in list_of_files:
+    f = open( fileName)
+    raw = f.read()
+
+    final_words = []
+    word_sent_token = []
+    pos_tag_sent_list = []
+    lem_ready_sent_list = []
+    mlu_ready_list = []
+    sent_dict = collections.OrderedDict()
+    #tokenize
+    sent_tokenize_list = sent_tokenize(raw)#breaks raw text into list of its sentences
+    #breaks each sentence into a  list of its parts
+    for sent in sent_tokenize_list:
+        word_sent_token.append(word_tokenize(sent))# DOWNSIDE: breaks up contractions\
+    #creates list of tuples for each part of each sentence (word, pos_tag) 
+    for sent in word_sent_token:
+        pos_tag_sent_list.append(nltk.pos_tag(sent))
+    #print "POS_TAG"    
+    #print pos_tag_sent_list
+    '''
+    #pos_tag
+    for sent in pos_tag_sent_list:
+        #pos_tag_sent_list is a list of lists
+            #list_outside = sentences
+            #list_inside = (word,pos_tag)
+        for tuple_pair in sent:
+            word = tuple_pair[0]
+            pos_tag = tuple_pair[1]
+            #makes words lowercase
+            word = word.lower()
+            lem_ready_word_list.append((word, pos_tag))
+    print 'LEM_READY'
+    print lem_ready_word_list
+    '''
+     #pos_tag
+    for sent in pos_tag_sent_list:
+        lem_ready_word_list = []
+        for tuple_pair in sent:
+            word = tuple_pair[0]
+            pos_tag = tuple_pair[1]
+            #gives correct pos_tag
+            wordnet_pos = get_wordnet_pos(pos_tag)
+            #makes words lowercase
+            word = word.lower()
+            lem_ready_word_list.append((word, wordnet_pos, pos_tag))
+        lem_ready_sent_list.append(lem_ready_word_list)
+    #print lem_ready_sent_list
             
+     #lemmatize
+    for sent in lem_ready_sent_list:
+        lem_sent = []
+        for tuple_trip in sent:
+            word = tuple_trip[0]
+            wordnet_pos = tuple_trip[1]
+            pos_tag = tuple_trip[2]
+            #makes sure words with no equivalent wordnet pos_tag still get lemmatized
+            if wordnet_pos == '':
+                lem_word = wordnet_lemmatizer.lemmatize(word)
+                if type(lem_word) == unicode:
+                    lem_word = unidecode(lem_word)
+                lem_sent.append((word, lem_word, pos_tag))
+                #final_words.append((lem_word, pos_tag))
+            
+                #insert code to assign tags according to WordCategories.xlsx (ex: negatives, pronoun)
+        #general lemmatization
+            else:
+                lem_word = wordnet_lemmatizer.lemmatize(word, pos=wordnet_pos)
+                if type(lem_word) == unicode:
+                    lem_word = unidecode(lem_word)
+                lem_sent.append((word, lem_word, pos_tag))
+                #final_words.append((lem_word, pos_tag))
+        mlu_ready_list.append(lem_sent)
+    #print mlu_ready_list
+    
+    sent_object_list = []
+    
+    #iterates through sentence    
+    sent_count = 0
+    for sent in sent_tokenize_list:
+        sent_count += 1
+        word_sent_count = 0
+        for word_sent in mlu_ready_list:
+            word_sent_count += 1
+            if sent_count == word_sent_count:
+                sent_dict[sent] = word_sent
+                break
+    '''   
+        for word_sent in pos_tag_sent_list:
+            word_sent_count += 1
+            if sent_count == word_sent_count:
+                sent_dict[sent] = word_sent
+                break
+    '''
+    
+    for sent in sent_dict:
+        y = []
+        for tuple_trip in sent_dict[sent]:
+            word = tuple_trip[0]
+            lem_word = tuple_trip[1]
+            pos_tag = tuple_trip[2]
+            #makes each word into a word object
+            new_word = Word(word, lem_word, pos_tag)
+            #print new_word.get_word()
+            y.append(new_word)
+        #makes each sentence into a sentence object
+        new_sent = Sentence(sent, y)
+        sent_object_list.append(new_sent)
+    
+    #for sent in sent_object_list:
+        #print sent.get_sentence()
+    
+    morpheme_count = 0
+    sent_count = 0
+    for sent in sent_object_list:
+        sent_count += 1
+        #print "NEW SENTENCE"
+        #print "\n"
+        #iterates through the indecies of the word list in each sentence object
+        for i in range(1,(len(sent.get_words())-1)):
+            current_word = sent.get_words()[i].get_word()
+            prev_word = sent.get_words()[i-1].get_word()
+            next_word = sent.get_words()[i+1].get_word()
+            if i == 1:
+                #print current_word
+                #print sent.get_words()[i-1].get
+                #print sent.get_words[i-1]
+                
+                if sent.get_words()[i-1].get_pos_tag() == 'NNS':
+                    if prev_word == "pants" or prev_word == "clothes":
+                        #print "found pants or clothes"
+                        morpheme_count += 1
+                        morpheme1_list.append(prev_word)
+                    else:
+                        NNS_list.append(prev_word)
+                        #print 'found NNS'
+                        morpheme_count += 2
+                if sent.get_words()[i-1].get_pos_tag() == 'VBZ':
+                    if prev_word != 'does'and sent.get_words()[i-1].get_lem_word() != 'be':
+                        VBZ_list.append(prev_word)
+                        #print 'found VBZ'
+                        morpheme_count += 2
+                    else:
+                        morpheme1_list.append(prev_word)
+                        morpheme_count += 1
+                if sent.get_words()[i-1].get_pos_tag() == ('VBD' or 'VBN'):
+                    if sent.get_words()[i-1].get_lem_word() == 'be':
+                        morpheme_count += 1
+                        morpheme1_list.append(prev_word)
+                    if prev_word[-2:] =='ed':
+                        VBDN_list.append(prev_word)
+                        morpheme_count += 2
+                    else:
+                        morpheme1_list.append(prev_word)
+                        morpheme_count += 1
+                if sent.get_words()[i-1].get_pos_tag() == 'VBG':
+                    if prev_word[-3:] == 'ing':
+                        VBG_list.append(prev_word)
+                        morpheme_count += 2
+                    else:
+                        morpheme1_list.append(prev_word)
+                        morpheme_count += 1
+                else:
+                    morpheme1_list.append(prev_word)
+                    morpheme_count += 1
+            else:
+                if sent.get_words()[i].get_pos_tag() == 'POS':
+                    if current_word == "'s":
+                        if prev_word != 'let':
+                            morpheme1_list.append(current_word)
+                            morpheme_count += 1
+                if current_word == "'t":
+                    if prev_word != ("don" and "won"):
+                        morpheme_count += 1
+                        morpheme1_list.append(current_word)
+                if sent.get_words()[i].get_pos_tag() == 'NNS':
+                    if current_word == "pants" or current_word == "clothes":
+                        #print "found pants or clothes"
+                        morpheme_count += 1
+                        morpheme1_list.append(current_word)
+                    else:
+                        #print 'found NNS'
+                        NNS_list.append(current_word)
+                        morpheme_count += 2
+                if sent.get_words()[i].get_pos_tag() == 'VBZ':
+                    if current_word != 'does'and sent.get_words()[i].get_lem_word() != 'be':
+                        #print 'found VBZ'
+                        VBZ_list.append(current_word)
+                        morpheme_count += 2
+                    else:
+                        morpheme_count += 1
+                        morpheme1_list.append(current_word)
+                if sent.get_words()[i].get_pos_tag() == ('VBD' or 'VBN'):
+                    if sent.get_words()[i].get_lem_word() == 'be':
+                        morpheme_count += 1
+                        morpheme1_list.append(current_word)
+                    if current_word[-2:] =='ed':
+                        #print current_word
+                        VBDN_list.append(current_word)
+                        morpheme_count += 2
+                    else:
+                        morpheme_count += 1
+                        morpheme1_list.append(current_word)
+                if sent.get_words()[i].get_pos_tag() == 'VBG':
+                    if current_word[-3:] == 'ing':
+                        #print current_word
+                        morpheme_count += 2
+                        VBG_list.append(current_word)
+                    else:
+                        morpheme_count += 1
+                        morpheme1_list.append(current_word)
+                if current_word == ',':
+                    pass
+                if current_word == '.':
+                    pass
+                if current_word == '``':
+                    pass
+                if current_word == "''":
+                    pass
+                if current_word == '...':
+                    pass
+                    #print current_word, sent.get_words()[i].get_pos_tag()
+                elif sent.get_words()[i].get_pos_tag() != ('.' and ',' and  '``' and  "''" and ':'):
+                    morpheme_count += 1
+                    morpheme1_list.append(current_word)
+    MLU_dict[fileName] = (morpheme_count, sent_count, float(morpheme_count)/sent_count)
+
+for book in MLU_dict:
+    morpheme_count, sent_count, MLU = MLU_dict[book]
+    print book[32:-4]
+    print "Morphemes: ", morpheme_count
+    print "Sentences: ", sent_count
+    print "MLU: ", MLU
+    print '\n'
+    
+            
+print "Morpheme1: ", morpheme1_list
+
 print "Plural nouns: "
 print NNS_list
 
 print "3rd person singular present verbs: "
 print VBZ_list
 
-print "Regular past tense verbs: "
-print VBD_list
+#print "Regular past tense verbs: "
+#print VBD_list
 
 print "Gerund or present participle: "
 print VBG_list
 
-print "Past participle verb: "
-print VBN_list
+print "Regular past and Past participle verb: "
+print VBDN_list
 
 print "Possessive ending: "
 print POS_list
@@ -335,140 +490,11 @@ print POS_list
 print "Possessive wh-pronoun: "
 print WP_list
 
-print "Be words: "
-print be_list
+#print "Be words: "
+#print be_list        
 
 
-
-sent_object_list = []
-
-#iterates through sentence    
-sent_count = 0
-for sent in sent_tokenize_list:
-    sent_count += 1
-    word_sent_count = 0
-    for word_sent in mlu_ready_list:
-        word_sent_count += 1
-        if sent_count == word_sent_count:
-            sent_dict[sent] = word_sent
-            break
-'''   
-    for word_sent in pos_tag_sent_list:
-        word_sent_count += 1
-        if sent_count == word_sent_count:
-            sent_dict[sent] = word_sent
-            break
-'''
-
-for sent in sent_dict:
-    y = []
-    for tuple_trip in sent_dict[sent]:
-        word = tuple_trip[0]
-        lem_word = tuple_trip[1]
-        pos_tag = tuple_trip[2]
-        #makes each word into a word object
-        new_word = Word(word, lem_word, pos_tag)
-        #print new_word.get_word()
-        y.append(new_word)
-    #makes each sentence into a sentence object
-    new_sent = Sentence(sent, y)
-    sent_object_list.append(new_sent)
-
-#for sent in sent_object_list:
-    #print sent.get_sentence()
-
-morpheme_count = 0
-sent_count = 0
-for sent in sent_object_list:
-    sent_count += 1
-    #print "NEW SENTENCE"
-    #print "\n"
-    #iterates through the indecies of the word list in each sentence object
-    for i in range(1,(len(sent.get_words())-1)):
-        current_word = sent.get_words()[i].get_word()
-        prev_word = sent.get_words()[i-1].get_word()
-        next_word = sent.get_words()[i+1].get_word()
-        if i == 1:
-            #print current_word
-            #print sent.get_words()[i-1].get
-            #print sent.get_words[i-1]
-            
-            if sent.get_words()[i-1].get_pos_tag() == 'NNS':
-                if prev_word == "pants" or prev_word == "clothes":
-                    print "found pants or clothes"
-                    morpheme_count += 1
-                else:
-                    print 'found NNS'
-                    morpheme_count += 2
-            if sent.get_words()[i-1].get_pos_tag() == 'VBZ':
-                if prev_word != 'does'and sent.get_words()[i-1].get_lem_word() != 'be':
-                    print 'found VBZ'
-                    morpheme_count += 2
-                else:
-                    morpheme_count += 1
-            if sent.get_words()[i-1].get_pos_tag() == ('VBD' or 'VBN'):
-                if sent.get_words()[i-1].get_lem_word() == 'be':
-                    morpheme_count += 1
-                if prev_word[-2:] =='ed':
-                    print prev_word
-                    morpheme_count += 2
-                else:
-                    morpheme_count += 1
-            if sent.get_words()[i-1].get_pos_tag() == 'VBG':
-                if prev_word[-3:] == 'ing':
-                    print prev_word
-                    morpheme_count += 2
-                else:
-                    morpheme_count += 1
-            else:
-                morpheme_count += 1
-        else:
-            if sent.get_words()[i].get_pos_tag() == 'POS':
-                if current_word == "'s":
-                    if prev_word != 'let':
-                        morpheme_count += 1
-            if current_word == "'t":
-                if prev_word != ("don" and "won"):
-                    morpheme_count += 1
-            if sent.get_words()[i].get_pos_tag() == 'NNS':
-                if current_word == "pants" or current_word == "clothes":
-                    print "found pants or clothes"
-                    morpheme_count += 1
-                else:
-                    print 'found NNS'
-                    morpheme_count += 2
-            if sent.get_words()[i].get_pos_tag() == 'VBZ':
-                if current_word != 'does'and sent.get_words()[i].get_lem_word() != 'be':
-                    print 'found VBZ'
-                    morpheme_count += 2
-                else:
-                    morpheme_count += 1
-            if sent.get_words()[i].get_pos_tag() == ('VBD' or 'VBN'):
-                if sent.get_words()[i].get_lem_word() == 'be':
-                    morpheme_count += 1
-                if current_word[-2:] =='ed':
-                    print current_word
-                    morpheme_count += 2
-                else:
-                    morpheme_count += 1
-            if sent.get_words()[i].get_pos_tag() == 'VBG':
-                if current_word[-3:] == 'ing':
-                    print current_word
-                    morpheme_count += 2
-                else:
-                    morpheme_count += 1
-            else:
-                morpheme_count += 1
-print "Morphemes: ", morpheme_count
-print "Sentences: ", sent_count
-
-print "MLU: "
-print float(morpheme_count)/sent_count
-
-                        
-            
-                        
-            
+                    
             
                     
         #finds Verb/Adverb pairs and chooses a synonym to replace the adverb
